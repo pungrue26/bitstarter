c4cb1ff6c78d593c6e8cb7b39a5bab25fb4c3d81
Joonho Kim <pungrue26@gmail.com>
2014-01-08 07:12:01 +0000

    Re

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 var buf = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
-    response.send('Hello, World');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
